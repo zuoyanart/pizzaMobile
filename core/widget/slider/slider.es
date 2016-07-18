f7750cdef67621aff2e836ca4a5644a7f8825edf
左盐 <huabinglan@163.com>
2016-07-18 10:49:09 +0800

    添加slider es操作

---
+++ core/widget/slider/slider.es
@@ -0,0 +1,22 @@
+/**
+ * 图片轮转
+ * @param  {[type]} ( [description]
+ * @return {[type]}   [description]
+ */
+let slider = (options) => {
+    let self = {};
+    let defOptions = {
+      obj: ".slider",
+      time: 1000,
+    };
+    /**
+     * 初始化
+     * @return {[type]} [description]
+     */
+    self.init = () =>{
+
+    }
+    return self;
+};
+
+moduel.exports = slider;
--- demo/widget/slider/slider.js
+++ demo/widget/slider/slider.js
@@ -1,6 +1,27 @@
 define('slider/slider', function(require, exports, module) {
 
+  /**
+   * 图片轮转
+   * @param  {[type]} ( [description]
+   * @return {[type]}   [description]
+   */
   "use strict";
+  
+  var slider = function slider(options) {
+    var self = {};
+    var defOptions = {
+      obj: ".slider",
+      time: 1000
+    };
+    /**
+     * 初始化
+     * @return {[type]} [description]
+     */
+    self.init = function () {};
+    return self;
+  };
+  
+  moduel.exports = slider;
   //# sourceMappingURL=/widget/slider/slider.js.map
   
 
